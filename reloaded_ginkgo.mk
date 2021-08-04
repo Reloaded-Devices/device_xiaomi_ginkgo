dc55103ef20d76b003f132ba8a40396ba239f421
Aryaman Srivastava <aryamansrivastava895@gmail.com>
2021-08-04 12:55:47 +0530

    ginkgo: Update Redfin FP to August

--- reloaded_ginkgo.mk
+++ reloaded_ginkgo.mk
@@ -36,8 +36,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Fingerprint
-BUILD_FINGERPRINT := google/redfin/redfin:11/RQ2A.210505.003/7255357:user/release-keys
-BUILD_DESCRIPTION := redfin-user 11 RQ2A.210505 7255357 release-keys
+BUILD_FINGERPRINT := google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys
+BUILD_DESCRIPTION := redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
